eaf3dbea2b46492a598efb99cc1723d2ba606f09
Bozo the geek <bozolesv2@gmail.com>
2025-01-06 17:50:34 +0100

    patch(pixl): 0004-pixl-fix-for-mangohud.patch
    PINE Server is blocking game running if mangohud is activated, need to deactivate completely during build

--- pcsx2/VMManager.cpp
+++ pcsx2/VMManager.cpp
@@ -190,7 +190,7 @@ static u64 s_session_start_time = 0;
 
 static bool s_screensaver_inhibited = false;
 
-static PINEServer s_pine_server;
+//static PINEServer s_pine_server;
 
 static bool s_discord_presence_active = false;
 static time_t s_discord_presence_time_epoch;
@@ -394,7 +394,7 @@ void VMManager::Internal::CPUThreadShutdown()
 {
 	ShutdownDiscordPresence();
 
-	s_pine_server.Deinitialize();
+	//s_pine_server.Deinitialize();
 
 	Achievements::Shutdown(false);
 
@@ -3065,18 +3065,18 @@ const std::vector<u32>& VMManager::GetSortedProcessorList()
 
 void VMManager::ReloadPINE()
 {
-	if (EmuConfig.EnablePINE && (s_pine_server.m_slot != EmuConfig.PINESlot || s_pine_server.m_end))
-	{
-		if (!s_pine_server.m_end)
-		{
-			s_pine_server.Deinitialize();
-		}
-		s_pine_server.Initialize(EmuConfig.PINESlot);
-	}
-	else if ((!EmuConfig.EnablePINE && !s_pine_server.m_end))
-	{
-		s_pine_server.Deinitialize();
-	}
+//	if (EmuConfig.EnablePINE && (s_pine_server.m_slot != EmuConfig.PINESlot || s_pine_server.m_end))
+//	{
+//		if (!s_pine_server.m_end)
+//		{
+//			s_pine_server.Deinitialize();
+//		}
+//		s_pine_server.Initialize(EmuConfig.PINESlot);
+//	}
+//	else if ((!EmuConfig.EnablePINE && !s_pine_server.m_end))
+//	{
+//		s_pine_server.Deinitialize();
+//	}
 }
 
 void VMManager::InitializeDiscordPresence()
