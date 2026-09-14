23346ca7f19e706eb1baabbf638ce710bcc57c80
BOZO THE GEEK <bozolesv2@gmail.com>
2026-09-14 22:41:12 +0200

    patch(pixl): 0005-version-fix-for-retroachievements.patch

--- pcsx2/BuildVersion.cpp
+++ pcsx2/BuildVersion.cpp
@@ -5,12 +5,12 @@
 
 namespace BuildVersion
 {
-	const char* GitTag = "v2.5.229";
+	const char* GitTag = "v2.8.2";
 	bool GitTaggedCommit = 1;
 	int GitTagHi = 2;
-	int GitTagMid = 5;
-	int GitTagLo = 229;
-	const char* GitRev = "v2.5.229";
-	const char* GitHash = "8fb2940f25876d404b0a7b9ec39d55b2b3abe9b9";
+	int GitTagMid = 8;
+	int GitTagLo = 2;
+	const char* GitRev = "v2.8.2";
+	const char* GitHash = "fd9d310ccbb6b8b62c976da8886a3c8fd3a10ff3";
 	const char* GitDate = GIT_DATE;
 } // namespace BuildVersion
